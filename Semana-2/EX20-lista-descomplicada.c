141bf899969d5ec75006a5280fd91c1bb1f1ba0e
Henrique-Versiani <henriquecversiani@gmail.com>
2023-06-21 16:40:24 -0300

    Ex 20 lista descomplicada - status: não sei porque, mas não funciona

--- Semana-2/EX20-lista-descomplicada.c
+++ Semana-2/EX20-lista-descomplicada.c
@@ -9,6 +9,8 @@ distancia entre elas.
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 typedef struct coordenanda{
     int x;
@@ -62,12 +64,50 @@ int menu ( void )
     return escolha;
 }
 
+celula *apagaLista ( celula *lista )
+{
+    celula *p = lista, *temp = NULL;
+    while ( p != NULL ){
+        temp = p->prox;
+        free( p );
+        p = temp;
+    }
+    return NULL;
+}
+
+float distancia ( int cid_1, int cid_2, celula *lista )
+{
+    int i = 0;
+    celula *p, *q;
+    float dist;
+    for ( p = lista->prox; p != NULL && i != cid_1; p = p->prox, i++ );
+    for ( q = lista->prox; q != NULL && i != cid_2; q = q->prox, i++ );
+
+    dist = sqrt ( pow ( p->local.x - q->local.x ,2 ) + pow ( p->local.y - q->local.y ,2 ) );
+    return dist;
+}
+
 int main ( void )
 {
     celula *lista = cria_lista( );
     char nome[50];
     coord local;
-    int mainmenu;
+    int mainmenu, cid1, cid2, N, i;
+    float dist;
+
+    printf ( "Digite a quantidade de cidades a serem inseridas: " );
+    scanf  ( "%d", &N );
+    getchar();
+    for ( i = 0; i < N; i++ ){
+        printf ( "\nDigite o nome da cidade: ");
+        scanf  ( "%[^\n]s", nome );
+        printf ( "\nDigite a coordenada x: ");
+        scanf  ( "%d", &local.x );
+        printf ( "\nDigite a coordenada y: ");
+        scanf  ( "%d", &local.y );
+        getchar();
+        insere_no_fim (nome, local, lista );
+    }
     for ( ;; ){
         mainmenu = menu( );
         switch( mainmenu ){
@@ -75,9 +115,17 @@ int main ( void )
                 imprime( lista );
                 break;
             case 2:
+                printf ( "\nDigite o numero da cidade 1: ");
+                scanf  ( "%d", &cid1 );
+                printf ( "\nDigite o numero da cidade 2: ");
+                scanf  ( "%d", &cid2 );
+                dist = distancia( cid1, cid2, lista );
+                printf ( "\nA distancia entre as duas cidades eh: %.2f", dist );
                 break;
             case 3:
+                lista = apagaLista( lista );
                 exit ( 0 );
         }
     }
+    return 0;
 }
