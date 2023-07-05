b9e45964ddfe83dd6c6ef1a6ad6323a2c0c69c90
Henrique-Versiani <henriquecversiani@gmail.com>
2023-07-05 14:16:37 -0300

    746 e 206 leet code

---
+++ Semana-4/206-Leet_Code.c
@@ -0,0 +1,16 @@
+struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode *headReverse = ( struct ListNode * )malloc( sizeof( struct ListNode ) );
+    struct ListNode *curr = head;
+    headReverse->next = NULL;
+
+    while (curr != NULL)
+    {
+        struct ListNode *new = ( struct ListNode * )malloc( sizeof( struct ListNode ) );
+        new->val = curr->val;
+        new->next = headReverse->next;
+        headReverse->next = new;
+        curr = curr->next;
+    }
+
+    return headReverse->next;
+}
